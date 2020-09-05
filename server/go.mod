4a8e7b9a94d660c540eab9c72d5d7f52acc13ea1
Izra Faturrahman <Frizz925@hotmail.com>
2020-09-05 18:22:31 +0700

    server: Write test case for Mongo repository

--- server/go.mod
+++ server/go.mod
@@ -14,6 +14,7 @@ require (
 	github.com/sirupsen/logrus v1.6.0
 	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
 	github.com/spf13/cobra v1.0.0
+	github.com/stretchr/testify v1.6.1
 	github.com/vmihailenco/msgpack/v5 v5.0.0-beta.1
 	go.mongodb.org/mongo-driver v1.4.0
 	golang.org/x/sys v0.0.0-20200831180312-196b9ba8737a // indirect
---
+++ server/repositories/MongoRepository_test.go
@@ -0,0 +1,57 @@
+package repositories
+
+import (
+	"air-sync/models"
+	"context"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func TestMongoRepository(t *testing.T) {
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		t.Fatal("Requires MONGODB_URI env to test")
+		return
+	}
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	require.Nil(t, err)
+	db := client.Database("airsync-test")
+	require.Nil(t, db.Drop(ctx))
+
+	sessionRepo := NewSessionMongoRepository(ctx, db)
+	attachmentRepo := NewAttachmentMongoRepository(ctx, db)
+
+	attachment, err := attachmentRepo.Create(models.CreateAttachment{})
+	require.Nil(t, err)
+
+	session, err := sessionRepo.Create()
+	require.Nil(t, err)
+	_, err = sessionRepo.InsertMessage(session.ID, models.InsertMessage{})
+	require.Nil(t, err)
+
+	session, err = sessionRepo.Create()
+	require.Nil(t, err)
+	insert := models.InsertMessage{}
+	insert.AttachmentID = attachment.ID
+	_, err = sessionRepo.InsertMessage(session.ID, insert)
+	require.Nil(t, err)
+	_, err = sessionRepo.InsertMessage(session.ID, insert)
+	require.Nil(t, err)
+	_, err = sessionRepo.InsertMessage(session.ID, models.InsertMessage{})
+	require.Nil(t, err)
+
+	found, err := sessionRepo.Find(session.ID)
+	require.Nil(t, err)
+	require.Equal(t, session.ID, found.ID)
+	require.Equal(t, 3, len(found.Messages))
+	require.GreaterOrEqual(t, found.Messages[0].CreatedAt, found.Messages[1].CreatedAt)
+	require.Empty(t, found.Messages[0].AttachmentID)
+	require.Equal(t, attachment.ID, found.Messages[1].AttachmentID)
+}
