5178b9c96c234795256af9709545bd314552f62e
Izra Faturrahman <Frizz925@hotmail.com>
2020-09-07 08:00:04 +0700

    ops: Use version for GAE deployment

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,12 @@
 set -e
 
 SCRIPT_DIR=$(realpath $(dirname $0))
+VERSION="v1"
 
 bash $SCRIPT_DIR/build.sh
 
 echo "Deploying service to Google App Engine..."
 cd $SCRIPT_DIR/server
-gcloud app deploy -q
-gcloud app deploy -q cron.yaml
+gcloud app deploy -q -v "$VERSION" app.yaml
+gcloud app deploy -q -v "$VERSION" cron.yaml
 echo "Service deployed."
